83ff3eb6a42d8a4bed201bbe05de5c75b108c25f
av.timofeev <av.timofeev@skbkontur.ru>
2020-06-23 17:56:04 +0500

    EnumExtensions

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace miscellanea.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToStringValue(this Enum value)
+        {
+            return value
+                .GetType()
+                .GetMember(value.ToString())
+                .FirstOrDefault()
+                ?.GetCustomAttributes(false)
+                .OfType<EnumMemberAttribute>()
+                .FirstOrDefault()
+                ?.Value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace miscellanea
+﻿using System;
+using System.Runtime.Serialization;
+using miscellanea.Extensions;
+
+namespace miscellanea
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine($"TestEnum - {TestEnum.None.ToStringValue()}");
         }
+    }
+    
+    public enum TestEnum
+    {
+        [EnumMember(Value = "None")] None = 0
     }
 }
